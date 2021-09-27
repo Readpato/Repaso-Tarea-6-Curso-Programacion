6f1ed14567565186d682aa59390aa6ff7442205f
Readpato <87768087+Readpato@users.noreply.github.com>
2021-09-27 16:19:05 +0200

    Archivos Iniciales Creados
    Fueron creados los documentos iniciales para iniciar el repaso de la tarea 6.

---
+++ tarea6a.js
@@ -0,0 +1,22 @@
+/*
+
+Pseudocodigo:
+
+- Crear un cuerpo de html en donde haya un form adentro.
+- Como instancia inicial, dentro del form crear un input que sea intendido en preguntar la cantidad de integrantes del grupo familiar,
+tambien crear un boton que pueda decir siguiente para avanzar el proceso.
+- Una vez ingresada la cantidad de personas, crear labels e inputs de la misma cantidad. (Pueden ser creado con .createElement!)
+- Agregar un boton (que estara escondido al principio)  - llamado "calcular" - que al ser interactuado calcule:
+
+    La mayor edad,
+    La menor edad,
+    El promedio del grupo familiar.
+
+Y que despues se puedan ver los resultados en un elemento pre-existente que esta escondido al principio (display: none) pero que despues aparezcan.
+
+Crear boton de resetear que empieze todo el proceso desde cero (display: 0 / .removeElement)
+
+Punto bonus! 
+Cuando el boton siguiente sea llamado, remover los integrantes pasados!
+
+*/
