1741d03004e3dfd1f12d116de0b82f0609644d57
Readpato <87768087+Readpato@users.noreply.github.com>
2021-09-27 16:52:10 +0200

    Creada la base HTML
    Creada la base esqueleto del codigo que vamos a trabajar y editar con JS

--- tarea6a.js
+++ tarea6a.js
@@ -2,9 +2,9 @@
 
 Pseudocodigo:
 
-- Crear un cuerpo de html en donde haya un form adentro.
-- Como instancia inicial, dentro del form crear un input que sea intendido en preguntar la cantidad de integrantes del grupo familiar,
-tambien crear un boton que pueda decir siguiente para avanzar el proceso.
+- Crear un body de html en donde haya un form adentro.
+- Como instancia inicial, dentro del form crear un input que pregunte la cantidad de integrantes del grupo familiar.
+- Crear un boton que pueda decir siguiente para avanzar el proceso.
 - Una vez ingresada la cantidad de personas, crear labels e inputs de la misma cantidad. (Pueden ser creado con .createElement!)
 - Agregar un boton (que estara escondido al principio)  - llamado "calcular" - que al ser interactuado calcule:
 
@@ -18,5 +18,8 @@ Crear boton de resetear que empieze todo el proceso desde cero (display: 0 / .re
 
 Punto bonus! 
 Cuando el boton siguiente sea llamado, remover los integrantes pasados!
+
+Notas:
+    - Sera mejor intentar de escribir el cuerpo osea HTML y CSS, y despues de eso agregarle la funcionalidad con JS?
 
 */
