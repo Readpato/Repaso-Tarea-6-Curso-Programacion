392842710ad89d398270b5876861e367d1d01e2d
Readpato <87768087+Readpato@users.noreply.github.com>
2021-09-28 20:01:53 +0200

    Agregadas Funciones en JS
    Fueron declaradas la mayoria de las funciones que crean nuevos elementos en el codigo HTML, que los eliminan. Queda en proceso todavia agregar la funcion resetear que inicia desde el principio el proceso.

--- tarea6a.js
+++ tarea6a.js
@@ -25,7 +25,15 @@ Notas:
 */
 
 
-//Acciones que sucede cuando el boton siguiente es clickeado
+/*
+
+Acciones que sucede cuando el boton siguiente es clickeado
+-Borrar los casilleros de los integrantes anteriores (si es que existen)
+-Crear las casillas de los integrantes
+-Mostrar el boton de calculo.
+
+*/
+
 document.querySelector('.botonSiguiente').onclick =  function(event) {
 
 const $cantidadIntegrantes = document.querySelector('.cantidadIntegrantes').value;
@@ -37,7 +45,14 @@ event.preventDefault();
 }
 
 //Funcion que elimina los integrantes de la ejecucion pasada del ejercicio
-function eliminarIntegrantesAnteriores() {}
+function eliminarIntegrantesAnteriores() {
+    
+    const $integrantes = document.querySelectorAll('.integrantes');
+    
+    for (let m = 0; m < $integrantes.length; m++ ) {
+        $integrantes[m].remove();
+    }
+}
 
 
 //Funcion que crea la cantidad de casilleros de los integrantes
@@ -45,9 +60,9 @@ function crearIntegrantes(cantidadIntegrantes) {
 
     if (cantidadIntegrantes > 0) {
         mostrarBotonCalculo();
-        mostrarIntegrantes();
+        mostrarIntegrantes(); //cambia el display
     } else {
-        resetear();
+        //resetear(); 
     }
 
     for( let i = 0;  i < cantidadIntegrantes; i++){
@@ -55,9 +70,68 @@ function crearIntegrantes(cantidadIntegrantes) {
     }
 }
 
+//Funcion que crea los elementos div, input y label en el body html
+
+function crearCasilleroIntegrantes(indice) {
+    const $casillerosIntegrantes = document.querySelector('#casillerosIntegrantes');
+    
+    let $nuevoDiv = document.createElement('div');
+    $nuevoDiv.className = 'integrantes'
+
+    let $label = document.createElement('label');
+    $label.textContent = `Integrante Familiar ${indice + 1}`;
+
+    let $input = document.createElement('input');
+    $input.type = 'number';
+
+    $nuevoDiv.appendChild($label);
+    $nuevoDiv.appendChild($input);
+    $casillerosIntegrantes.appendChild($nuevoDiv);
+}   
+
+
 //Funcion que muestra el boton calculo
 
 function mostrarBotonCalculo() {
     document.querySelector('#calculoEdades').className = '';
+}
+
+//Funcion que oculta el boton de calculo
+
+function ocultarbotonCalculo() {
+    document.querySelector('#calculoEdades').classname = 'oculto';
+}
+
+//Funcion que muestra los casilleros de los integrantes
+
+function mostrarIntegrantes() {
+    document.querySelector('#casillerosIntegrantes').className = '';
+}
+
+// Funcion que oculta los integrantes
+
+function ocultarIntegrantes() {
+    document.querySelector('#casillerosIntegrantes').className = 'oculto';
+}
+
+
+//Funcion que oculta el texto de los resultados
+
+function ocultarTextoResultado() {
+}
+
+/*
+Funcion resetear
+-Borrar los integrantes anteriores eliminarIntegrantesAnteriores();
+- Ocultar los botones de calculo
+- Ocultar los integrantes
+- Ocultar texto resultados
+
+*/
+
+
+function resetear() {
+    eliminarIntegrantesAnteriores();
+
 }
 
