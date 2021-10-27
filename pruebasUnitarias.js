b13819633a3aa7488ea054c7ce6785acd2660fd7
Readpato <87768087+Readpato@users.noreply.github.com>
2021-10-27 18:24:53 +0200

    Actualizado pruebasunitarias.js
    Fue actualizado el archivo pruebas unitarias para que demuestre correctamente los errores del console.assert.

--- pruebasUnitarias.js
+++ pruebasUnitarias.js
@@ -1,6 +1,6 @@
-// console.assert(validarIntegrantesFamiliares('') === 'No ingresaste ningun caracter.',
-//     'Validar Integrantes Familiares no valido que se no se hayan ingresado caracteres'
-// )
+console.assert(validarIntegrantesFamiliares('') === 'No ingresaste ningun caracter.',
+    'Validar Integrantes Familiares no valido que se no se hayan ingresado caracteres'
+)
 
 console.assert(validarIntegrantesFamiliares('a') === 'Ingresaste un caracter invalido.',
     'Validar Integrantes Familiares no valido que se hayan ingresado caracteres validos'
